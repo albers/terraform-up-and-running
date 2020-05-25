fb666452740236c09fd18488c641dcf459a2f0bf
Harald Albers <github@albersweb.de>
2020-05-25 14:03:52 +0200

    A production environment with slightly different settings is created.

---
+++ code/terraform/04-terraform-modules/module-example/prod/services/webserver-cluster/main.tf
@@ -0,0 +1,17 @@
+provider "aws" {
+  region  = "us-east-2"
+  version = "~> 2.62"
+}
+
+module "webserver_cluster" {
+  source = "../../../modules/services/webserver-cluster"
+
+  cluster_name = "webservers-prod"
+  db_remote_state_bucket = "terraform-book-state"
+  db_remote_state_key = "module-example/prod/data-stores/mysql/terraform.tfstate"
+  db_remote_state_region = "us-east-2"
+
+  instance_type = "t2.small"
+  min_size = 2
+  max_size = 10
+}
